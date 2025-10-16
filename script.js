58f50719b736e8e9d6fa59b5d7b1393240240711
DevKumar8057 <devkumar8057600@gmail.com>
2025-10-16 09:40:59 +0530

    Day 2

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-document.writeln("Hello")
-console.log(writeln)
+console.log("Hello!")
+let a=10;
+console.log(a);
+
+
+/**********/
+var name="dev";
+var age=27;
+console.log(name);
+console.log(age);
+console.log("my name is " +name+ " and my age is " +age);
+
+
+//console.log('my name is ${name} and my age is ${age}');
+
+
+/************** */
+
+
+let b="delhi";
+b="lucknow";
+console.log(b)
+
+/***************** */
+
+let s=true;
+console.log(s);
+console.error("this is error");
+
+console.warn("this is warning");
+console.info("this is info");
+console.debug("this is debug");
+
+console.table({name:"dev",age:27,city:"delhi"});
+//console.clear();
+
+
+for(let i=0;i<3;i++){
+    console.count("loop executed: ")  //count work as i
+}
